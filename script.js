b558cac2d9209242fa4267814a22c97d4e162227
Olashile-Tech <olashileuthman47@gmail.com>
2026-04-05 11:35:29 +0100

    updated button

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-function changeColor() {
-  const colors = ["#f1f2f6", "#dfe4ea", "#ced6e0", "#a4b0be"];
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  document.body.style.backgroundColor = randomColor;
+function openWhatsApp() {
+  window.open("https://wa.me/2347026788055", "_blank");
 }
